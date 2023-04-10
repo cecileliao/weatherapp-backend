77455ca809f5a0d61c6b5b4ac688db4655aaf295
Your name <Your email>
2023-04-10 11:46:17 +0200

    weather part4 .env

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://cecileliao:CapsuleBatch77@capsule.ytksgmn.mongodb.net/weatherpart4';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
